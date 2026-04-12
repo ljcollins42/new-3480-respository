506160ee41ca72fd993c4e503d9f598b055b13fb
ljcollins42 <lj_collins@icloud.com>
2026-04-12 13:36:22 -0400

    Added third enemy type

---
+++ Downloads/new-3480-respository-olivia-2/new-3480-respository-olivia-2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyOnePrefab;
+    public GameObject cloudPrefab;
+    public GameObject enemyTwoPrefab;
+    public GameObject enemyThreePrefab;
+    public GameObject enemyExplosionPrefab;
+
+    public TextMeshProUGUI livesText;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6f;
+        score = 0;
+        if (enemyExplosionPrefab == null && enemyOnePrefab != null)
+        {
+            Enemy enemyOneScript = enemyOnePrefab.GetComponent<Enemy>();
+            if (enemyOneScript != null && enemyOneScript.explosionPrefab != null)
+            {
+                enemyExplosionPrefab = enemyOneScript.explosionPrefab;
+            }
+        }
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
+        InvokeRepeating("SecondEnemy",2,3);
+        InvokeRepeating("ThirdEnemy",3,3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemy()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0);
+        SpawnEnemy(enemyOnePrefab, spawnPosition, Quaternion.Euler(0, 1800, 0));
+    }
+
+    void SecondEnemy()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 1.5f, verticalScreenSize, 0);
+        SpawnEnemy(enemyTwoPrefab, spawnPosition, Quaternion.Euler(0, 1800, 0));
+    }
+
+    void ThirdEnemy()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 2.0f, verticalScreenSize,0);
+        SpawnEnemy(enemyThreePrefab, spawnPosition, Quaternion.Euler(0,180,0));
+    }
+
+    GameObject SpawnEnemy(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        GameObject enemy = Instantiate(prefab, position, rotation);
+        ConfigureEnemy(enemy);
+        return enemy;
+    }
+
+    void ConfigureEnemy(GameObject enemy)
+    {
+        if (enemy == null)
+        {
+            return;
+        }
+
+        Enemy enemyScript = enemy.GetComponent<Enemy>();
+        if (enemyScript == null)
+        {
+            enemyScript = enemy.AddComponent<Enemy>();
+        }
+
+        if (enemyScript.explosionPrefab == null)
+        {
+            enemyScript.explosionPrefab = enemyExplosionPrefab;
+        }
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
+    }
+
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
+}
---
+++ Downloads/new-3480-respository-olivia-2/new-3480-respository-olivia-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int lives;
+    private float speed;
+
+    private GameManager gameManager;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    public GameObject bulletPrefab;
+    public GameObject bullet2Prefab;
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        lives = 3;
+        speed = 5.0f;
+        gameManager.ChangeLivesText(lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+    }
+
+    public void LoseALife()
+    {
+        //lives = lives - 1;
+        //lives -= 1;
+        lives--;
+        gameManager.ChangeLivesText(lives);
+        if (lives == 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Shooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(bullet2Prefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
+
+        float horizontalScreenSize = gameManager.horizontalScreenSize;
+        float screenVerticalSize = gameManager.verticalScreenSize;
+        float topHalfThreshold = screenVerticalSize / 5f;
+
+        if (transform.position.x <= -horizontalScreenSize || transform.position.x > horizontalScreenSize)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+
+        if (transform.position.y >= topHalfThreshold)
+        {
+            transform.position = new Vector3(transform.position.x, -screenVerticalSize, 0f);
+        }
+        else if (transform.position.y <= -screenVerticalSize)
+        {
+            transform.position = new Vector3(transform.position.x, topHalfThreshold, 0f);
+        }
+
+    }
+}
