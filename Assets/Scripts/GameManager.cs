664a9661a71da09dcb14039a5f5eabed91f0900a
ljcollins42 <lj_collins@icloud.com>
2026-04-22 20:08:12 -0400

    m

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,174 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
+    public GameObject enemyThreePrefab;
+    public GameObject cloudPrefab;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject powerupPrefab;
+    public GameObject audioPlayer;
+    public GameObject coinPrefab;
+
+    public AudioClip powerupSound;
+    public AudioClip powerdownSound;
+
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI powerupText;
+    
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
+    public int cloudMove;
+
+    private bool gameOver;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = audioPlayer.GetComponent<AudioSource>();
+
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        cloudMove = 1;
+        gameOver = false;
+        AddScore(0);
+
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
+        InvokeRepeating("SecondEnemy", 2, 3);
+        InvokeRepeating("ThirdEnemy", 3, 3);
+        StartCoroutine(SpawnPowerup());
+        StartCoroutine(Coin());
+        powerupText.text = "No powerups yet!";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void SecondEnemy()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void ThirdEnemy()
+    {
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void CreatePowerup()
+    {
+        Instantiate(powerupPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+
+    void CreateCoin()
+    {
+         Instantiate(coinPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
+    }
+
+    public void ManagePowerupText(int powerupType)
+    {
+        switch (powerupType)
+        {
+            case 1:
+                powerupText.text = "Speed!";
+                break;
+            case 2:
+                powerupText.text = "Double Weapon!";
+                break;
+            case 3:
+                powerupText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerupText.text = "Shield!";
+                break;
+            default:
+                powerupText.text = "No powerups yet!";
+                break;
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        float spawnTime = Random.Range(3, 5); 
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerup();
+        StartCoroutine(SpawnPowerup());
+    }
+
+
+     IEnumerator Coin()
+    {
+        float spawnTime = Random.Range(3, 5); 
+        yield return new WaitForSeconds(spawnTime);
+        CreateCoin();
+        StartCoroutine(Coin());
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1: audioSource.PlayOneShot(powerupSound); break;
+            case 2: audioSource.PlayOneShot(powerdownSound); break;
+        }
+    }
+
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        restartText.SetActive(true);
+        gameOver = true;
+        CancelInvoke();
+        cloudMove = 0;
+    }
+
+}
